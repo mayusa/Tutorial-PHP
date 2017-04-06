2f7cc2169cdf6077b7fb395b80996f18a5a675e6
MayLiu <ashucn@gmail.com>
2017-04-06 16:56:57 -0700

    Update

--- PDO/index.php
+++ PDO/index.php
@@ -2,5 +2,5 @@
 $database = require 'bootstrap.php';
 
 $tasks = $database->selectAll('todos');
-
+var_dump($tasks);
 require 'index.view.php';
--- PDO/index.view.php
+++ PDO/index.view.php
@@ -1,3 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php foreach ($tasks as $task) : ?>
+		<li>
+			<?= $task->description; ?>
+		</li>
 
-
+	<?php endforeach; ?>
+</body>
+</html>
