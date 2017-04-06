90413c792e3b33933ec99612251243fd97c9c953
MayLiu <ashucn@gmail.com>
2017-04-06 16:35:28 -0700

    Update

--- PDO/bootstrap.php
+++ PDO/bootstrap.php
@@ -3,7 +3,7 @@
 require 'database/Connection.php';
 require 'database/QueryBuilder.php';
 $config = require 'config.php';
-// var_dump($config['database']);
+
 return new QueryBuilder(
 	Connection::make($config['database'])
 );
--- PDO/index.view.php
+++ PDO/index.view.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>tasks</title>
-</head>
-<body>
-	test
-</body>
-</html>
+<?php
+
+
